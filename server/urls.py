5d609209eafcffb578d878e814042cfe94593009
ryanreh99 <ryanrehman99@gmail.com>
2020-10-01 02:59:04 +0530

    Redirect to homepage on signin/signup.

--- server/urls.py
+++ server/urls.py
@@ -6,6 +6,7 @@ app_name = 'server'
 
 urlpatterns = [
     path('', task_list, name='task-list'),
+    path('accounts/profile/', task_list, name='task-list'),
     path('create', create_task, name='create-task'),
     path('getTask', get_task, name='get-task'),
 ]
--- sprint_board/urls.py
+++ sprint_board/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include('server.urls', namespace='server')),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', include('server.urls', namespace='server')),
 ]
