09088649629c68196db709f6f6d7ef94278d4002
ryanreh99 <ryanrehman99@gmail.com>
2020-12-26 17:23:01 +0530

    Migrate to SASS using webpack.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # sprint-board
 
-A WIP project where users can upload tasks or accept them. (Upwork clone)
+A WIP project where users can upload tasks or accept them.
 
 ![demo](https://user-images.githubusercontent.com/40122794/96768033-f51cf700-13fa-11eb-9e13-905b5679870c.gif)
 
@@ -12,9 +12,10 @@ A WIP project where users can upload tasks or accept them. (Upwork clone)
 - Javascript + JQuery for frontend.
 
 ### Setup-
-- Activate virtual environment.
-- `npm install` and `pip install Django` and `pip install django-allauth`
-- `python manage.py collectstatic`
+- [`Activate the virtual environment.`](https://docs.python.org/3/tutorial/venv.html)
+- `pip install -r requirements.txt`
+- `npm install`
+- `npm run-script build`
 - `npm run-script start-server`
 - `python manage.py runserver` (In a seperate terminal)
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,37 @@
 const path = require('path');
 
-const BundleTracker = require('webpack-bundle-tracker')
+const BundleTracker = require('webpack-bundle-tracker');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   context: __dirname,
   entry: [
-    './static/js/main.js'
+    './static/js/main.js',
+    './static/css/style.scss',
   ],
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
     publicPath: 'http://localhost:9000/',
   },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Extracts a CSS file per JS string
+          MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
   plugins: [
     new BundleTracker({filename: './webpack-stats.json'}),
+    new MiniCssExtractPlugin(),
   ],
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
