3a3042359a8f95cd6f1c9134b33a7862d416d249
ryanreh99 <ryanrehman99@gmail.com>
2020-12-26 21:46:08 +0530

    Update prettier config.

--- webpack.config.js
+++ webpack.config.js
@@ -6,56 +6,56 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const RemoveEmptyScriptsPlugin = require("webpack-remove-empty-scripts");
 
 const source_map_options = {
-    options: {
-        sourceMap: true,
-    },
+  options: {
+    sourceMap: true,
+  },
 };
 
 module.exports = {
-    context: __dirname,
-    entry: {
-        main: "./static/js/app.js",
-        style: ["./static/css/bootstrap.scss", "./static/css/style.scss"],
-    },
-    output: {
-        filename: "[name].js",
-        path: path.resolve(__dirname, "dist"),
-        publicPath: "http://localhost:9000/",
-    },
-    devtool: "source-map",
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    // Extracts a CSS file per JS string
-                    MiniCssExtractPlugin.loader,
-                    // Translates CSS into CommonJS
-                    {
-                        loader: "css-loader",
-                        ...source_map_options,
-                    },
-                    // Compiles Sass to CSS
-                    {
-                        loader: "sass-loader",
-                        ...source_map_options,
-                    },
-                ],
-            },
+  context: __dirname,
+  entry: {
+    main: "./static/js/app.js",
+    style: ["./static/css/bootstrap.scss", "./static/css/style.scss"],
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "http://localhost:9000/",
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Extracts a CSS file per JS string
+          MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          {
+            loader: "css-loader",
+            ...source_map_options,
+          },
+          // Compiles Sass to CSS
+          {
+            loader: "sass-loader",
+            ...source_map_options,
+          },
         ],
-    },
-    plugins: [
-        new BundleTracker({ filename: "./webpack-stats.json" }),
-        new MiniCssExtractPlugin(),
-        new RemoveEmptyScriptsPlugin(),
-        new webpack.ProvidePlugin({
-            $: "jquery",
-            jQuery: "jquery",
-        }),
+      },
     ],
-    devServer: {
-        contentBase: path.join(__dirname, "dist"),
-        compress: true,
-        port: 9000,
-    },
+  },
+  plugins: [
+    new BundleTracker({ filename: "./webpack-stats.json" }),
+    new MiniCssExtractPlugin(),
+    new RemoveEmptyScriptsPlugin(),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+  },
 };
